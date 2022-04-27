2b33e5266c8083b2b4f9129f1cfad376cc204792
Berna <93603466+bernase@users.noreply.github.com>
2022-04-27 21:45:46 +0300

    Add files via upload

---
+++ 4_autoassoc_1.py
@@ -0,0 +1,48 @@
+from sklearn.neural_network import MLPRegressor
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import random
+from scipy import signal
+
+x = np.linspace(0, 1, 5000, endpoint=True)
+
+y = [signal.square(2 * np.pi * 5 * i) + 0.2*random.gauss(0,1) for i in x]
+n = len(y)
+window = 20
+nn = n - window + 1
+P = np.empty((0,window))
+
+for i in range(0, nn):
+  a = y[i : i+window]
+  P = np.vstack([P, a])
+print(np.shape(P))
+
+model = MLPRegressor(activation='tanh',hidden_layer_sizes=(10,),verbose=True,learning_rate='adaptive',max_iter=1000)
+model.fit(P, P)
+yy = model.predict(P)
+
+out = np.zeros(len(x))
+
+for i in range(0,yy.shape[0]):
+  for j in range(0,window):
+    out[i+j] += yy[i][j]
+
+k = 1
+sz = len(out)
+for i in range(0,sz):
+  out[i] = out[i]/k
+  
+  if k == window and window <= sz - i:
+    k = window
+  elif window > sz - i:
+    k = k - 1 
+  else:
+    k = k + 1
+
+plt.plot(x,y)
+plt.show()
+
+plt.plot(x, out)
+plt.show()
