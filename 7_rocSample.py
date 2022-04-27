8ca68fc81216d87035fe02e1db893fe8309d8cd8
Berna <93603466+bernase@users.noreply.github.com>
2022-04-27 21:48:27 +0300

    Add files via upload

---
+++ 7_rocSample.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from pandas import read_csv
+from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+#import dataset from CSV file on Github
+url = "https://raw.githubusercontent.com/Statology/Python-Guides/main/default.csv"
+data = pd.read_csv(url)
+
+#define the predictor variables and the response variable
+X = data[['student', 'balance', 'income']]
+y = data['default']
+
+#split the dataset into training (70%) and testing (30%) sets
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3) 
+
+#instantiate the model
+log_regression = LogisticRegression()
+
+#fit the model using the training data
+log_regression.fit(X_train,y_train)
+#define metrics
+y_pred_proba = log_regression.predict_proba(X_test)[:,1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+#create ROC curve
+plt.plot(fpr,tpr,label="AUC=" + str(auc))
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.legend(loc=4)
+plt.show()
